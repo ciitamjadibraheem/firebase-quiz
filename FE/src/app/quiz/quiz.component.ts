78b6197da2410ac3ae2d7b275ac6a8a879f5fa0b
amjad <amjad.ibraheeem@gmail.com>
2025-08-18 15:29:11 +0200

    add firebase and save data to firestore

--- FE/src/app/quiz/quiz.component.ts
+++ FE/src/app/quiz/quiz.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {NgIf} from "@angular/common";
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-quiz',
@@ -17,19 +18,42 @@ export class QuizComponent {
   fullName: string = '';
   termsAccepted: boolean = false;
   showTermsModal: boolean = false;
+  loading: boolean = false;
+  showSuccessModal: boolean = false;
+  showErrorModal: boolean = false;
+
+
+  constructor(private firestore: Firestore) {}
 
   isFormValid(): boolean {
     return this.selectedAnswer !== null && this.fullName.trim().length > 0 && this.termsAccepted;
   }
 
-  submit() {
-    console.log('Answer:', this.selectedAnswer);
-    if (this.selectedAnswer === 'Other') {
-      console.log('Full Name:', this.fullName);
+  async submit() {
+    this.loading = true; // show loading overlay
+    try {
+      const submissions = collection(this.firestore, 'quizSubmissions');
+      await addDoc(submissions, {
+        answer: this.selectedAnswer,
+        fullName:  this.fullName,
+        timestamp: new Date(),
+      });
+
+      this.resetForm();
+      this.showSuccessModal = true;
+    } catch (error) {
+      this.showErrorModal = true;
+    } finally {
+      this.loading = false;
     }
-    alert('Submitted successfully!');
   }
 
+
+  resetForm() {
+    this.selectedAnswer = null;
+    this.fullName = '';
+    this.termsAccepted = false;
+  }
 
   openTerms() {
     this.showTermsModal = true;
@@ -37,5 +61,12 @@ export class QuizComponent {
 
   closeTerms() {
     this.showTermsModal = false;
+  }
+
+  closeSuccess() {
+    this.showSuccessModal = false;
+  }
+  closeError() {
+    this.showErrorModal = false;
   }
 }
---
+++ FE/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDSdObumCvoHIOP8Ao7TudPDs9cGJ--EzA",
+    authDomain: "ciit-firebase-quiz.firebaseapp.com",
+    projectId: "ciit-firebase-quiz",
+    storageBucket: "ciit-firebase-quiz.firebasestorage.app",
+    messagingSenderId: "575967383029",
+    appId: "1:575967383029:web:f4fd808377b5638ffbe4bc"  }
+};
